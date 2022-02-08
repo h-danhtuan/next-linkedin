b853e827764e577aa59d8ddade70a9cdbe5c6343
Cam Ninh <camninh224@gmail.com>
2022-02-08 22:02:24 +0700

    initial project, login page, next auth, signin func

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// main blue: #2977c9
+// 2nd blue: #0073b1
