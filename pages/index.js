e6ea51097e03ca66eabc67974e71d1f003ffa700
Cam Ninh <camninh224@gmail.com>
2022-02-11 11:53:41 +0700

    add feed,post, modal, form and react-recoil

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,27 @@
-import { signOut } from "next-auth/react";
+import { getSession, useSession } from "next-auth/react";
+import { useRecoilState } from "recoil";
+import { modalState, modalTypeState } from "../atoms/modalAtom";
 import Header from "../components/Header";
 import Head from "next/head";
 import SideBar from "../components/SideBar";
+import { useRouter } from "next/router";
+import Feed from "../components/Feed";
+import { AnimatePresence } from "framer-motion";
+import Modal from "../components/Modal";
 
 export default function Home() {
+  const [modalOpen, setModalOpen] = useRecoilState(modalState);
+  const [modalType, setModalType] = useRecoilState(modalTypeState);
+  const router = useRouter();
+
+  const { status } = useSession({
+    required: true,
+    onUnauthenticated() {
+      // The user is not authenticated, handle it here.
+      router.push("/home");
+    },
+  });
+
   return (
     <div className="bg-[#F3F2EF] dark:bg-black dark:text-white h-screen overflow-y-scroll md:space-y-6">
       <Head>
@@ -18,9 +36,33 @@ export default function Home() {
           <SideBar />
 
           {/* Feed */}
+          <Feed />
         </div>
         {/* Widgets */}
+
+        <AnimatePresence>
+          {modalOpen && (
+            <Modal handleClose={() => setModalOpen(false)} type={modalType} />
+          )}
+        </AnimatePresence>
       </main>
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  // Check if the user is authenticated on the server...
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/home",
+      },
+    };
+  }
+
+  return {
+    props: { session },
+  };
 }
